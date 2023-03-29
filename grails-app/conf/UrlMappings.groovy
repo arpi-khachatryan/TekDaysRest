c5c12659796486ebff0598ed6b2b965bc71eeb76
arpi-khachatryan <arpikhachatryan2@gmail.com>
2023-03-29 10:39:12 +0400

    Initial commit

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -6,53 +6,52 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-//
-//        "/api/$id?/$format?" {
-//            controller = 'tekEvent'
-//            action = [
-//                    POST  : 'create',
-//                    GET   : 'read',
-//                    PUT   : 'update',
-//                    DELETE: 'delete',
-//            ]
-//        }
-//
-//        "/api/$id?/$format?" {
-//            controller = 'tekUser'
-//            action = [
-//                    POST  : 'create',
-//                    GET   : 'read',
-//                    PUT   : 'update',
-//                    DELETE: 'delete',
-//            ]
-//        }
-//
-//        "/api/all/$format?" {
-//            controller = 'tekEvent'
-//            action = "readAll"
-//        }
-//
-//        "/api/all/$format?" {
-//            controller = 'tekUser'
-//            action = "readAll"
-//        }
-//
-//
-////            "/tekEvent/$id?"(controller:"tekEvent") {
-////                action = [GET:'show', PUT:'save', POST:'update', DELETE:'delete']
-////            }
-//
-//
-//
+
         "/"(view: "/index")
         "500"(view: '/error')
         "404"(view: '/notFound')
-//
-//
+
+        "/api/$id?/$format?" {
+            controller = 'tekEvent'
+            action = [
+                    POST  : 'createPost',
+                    GET   : 'create',
+                    GET   : 'getById',
+                    PUT   : 'update',
+                    DELETE: 'delete',
+            ]
+        }
+
+        "/api/$format?" {
+            controller = 'tekEvent'
+            action = "getAll"
+        }
+
+        "/api/$id?/$format?" {
+            controller = 'tekUser'
+            action = [
+                    POST  : 'createPost',
+                    GET   : 'create',
+                    GET   : 'getById',
+                    PUT   : 'update',
+                    DELETE: 'delete',
+            ]
+        }
+
+        "/api/$format?" {
+            controller = 'tekUser'
+            action = "getAll"
+        }
+
+//            "/tekEvent/$id?"(controller:"tekEvent") {
+//                action = [GET:'show', PUT:'save', POST:'update', DELETE:'delete']
+//            }
+
+
 ////        "/music/$artist/$album?/$song?"(controller:"store") {
 ////            action = [GET:'show', PUT:'save', POST:'update', DELETE:'delete']
 ////        }
-//
+
 ////        "/music/$artistName/$albumTitle?/$songTitle?"{
 ////            controller = {
 ////                if(params.albumTitle && params.songTitle) return 'song'
